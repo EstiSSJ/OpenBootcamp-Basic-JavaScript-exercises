e18e01d396328be68a5d26b34c88e5af72b841b3
EstiSSJ <117943176+EstiSSJ@users.noreply.github.com>
2022-12-24 15:52:34 -0300

    Update Sección 2, index.js

--- Sección 2, Ejercicio/Sección 2, index.js
+++ Sección 2, Ejercicio/Sección 2, index.js
@@ -1,1 +1,14 @@
+const datosPersonales = {
+    nombre: "Esteban",
+    edad: 18,
+    esDesarrollador: true,
+    birthDate: new Date(2004,01,12),
+    libroFav: {
+        titulo: "Hunger Games",
+        autor: "Suzanne Collins",
+        fecha: new Date(2008,08,14),
+        url: "https://www.amazon.com/Juegos-hambre-Hunger-Games-Spanish/dp/6073807848"
+    }
+};
 
+console.log(datosPersonales);
