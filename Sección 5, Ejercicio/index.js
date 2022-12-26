8a71f8326d64cbc5ef3eee08d22a2f2c0a037e70
EstiSSJ <117943176+EstiSSJ@users.noreply.github.com>
2022-12-26 14:24:31 -0300

    Create index.js

---
+++ Sección 5, Ejercicio/index.js
@@ -0,0 +1,10 @@
+const alturaCm = 170;
+const alturaM = alturaCm / 100;
+
+const pesoKg = 69.4;
+
+const alturaMArriba = Math.ceil(alturaM);
+const pesoKgAbajo = Math.floor(pesoKg);
+
+const maxValorJS = Number.MAX_VALUE;
+const esIgual = maxValorJS + 1 === maxValorJS;
