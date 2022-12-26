3a1f96b550e907fd7c453190b6aba2860915aa9c
EstiSSJ <117943176+EstiSSJ@users.noreply.github.com>
2022-12-26 20:53:45 -0300

    Update index.js

--- Sección 10, Ejercicio/index.js
+++ Sección 10, Ejercicio/index.js
@@ -1,5 +1,4 @@
 import * as controller from "./controller.js";
 import chalk from "chalk";
 
-console.log(controller.multiplica(controller.suma(1, 2), controller.suma(4, 5)));
 console.log(chalk.green(controller.multiplica(controller.suma(1, 2), controller.suma(4, 5))));
