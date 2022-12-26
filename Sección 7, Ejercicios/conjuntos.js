6c693560c1d8569762da21ea545e6406cc3eebf1
EstiSSJ <117943176+EstiSSJ@users.noreply.github.com>
2022-12-26 17:11:56 -0300

    Create conjuntos.js

---
+++ Sección 7, Ejercicios/conjuntos.js
@@ -0,0 +1,8 @@
+const nombresFamilia = new Set(["Padre", "Madre", "Hermana"]);
+
+nombresFamilia.add("Esteban");
+nombresFamilia.add("Esteban");
+
+nombresFamilia.add("Javascript");
+
+console.log(nombresFamilia)
