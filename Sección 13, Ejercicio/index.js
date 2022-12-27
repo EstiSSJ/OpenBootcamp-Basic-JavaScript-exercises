42480bbf6857103a3ee09dd00b53d87e196f458f
EstiSSJ <117943176+EstiSSJ@users.noreply.github.com>
2022-12-27 19:54:37 -0300

    Create index.js

---
+++ Sección 13, Ejercicio/index.js
@@ -0,0 +1,13 @@
+const nombre = "Esteban";
+const apellido = `Ribeiro`;
+
+const estudiante = nombre.concat(" ", apellido);
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const letrasInEstudiante = estudiante.length;
+const primerLetraNombre = nombre[0];
+const ultimaLetraApellido = apellido[apellido.length - 1];
+const estudianteSinEspacio = estudiante.replace(/ /g, '');
+const NombreInEstudiante = estudiante.includes(nombre);
+
+console.log(estudiante)
