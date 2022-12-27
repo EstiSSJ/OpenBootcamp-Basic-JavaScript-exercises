eb28a045c679942ddac26cb98a4deb8a13ac9241
EstiSSJ <117943176+EstiSSJ@users.noreply.github.com>
2022-12-27 20:57:04 -0300

    Create index.js

---
+++ Sección 14, Ejercicio/index.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", () => alert("click en el botón"))
+
+$("button").click(function() {
+    console.log("Hola, estoy utilizando jQuery")
+})
